37a1dc1a87e5b88832a828fbdfaff9ffc1d1932a
Dylan <dylancccq@qq.com>
2016-07-20 16:42:45 +0800

    config bug

--- TSScrollBtnDemo/TSScrollBtn/TSBtnLabel.h
+++ TSScrollBtnDemo/TSScrollBtn/TSBtnLabel.h
@@ -9,14 +9,14 @@
 #define TSbackH 200.f
 /**   背景的颜色    the color of backView      */
 #define TSBackColor [UIColor yellowColor]
-/**   每一个按钮的宽度   the width of button       */
-#define TSBtnLableBtnW 80.f
-/**   按钮的间距       the spide of every button    */
-#define TSBtnLableSpide 10.f
-/**   按钮的高度        the height of every button  */
-#define TSBtnLableBtnH 40.f
 /**   横向有几个按钮    the count of button in a line     */
 #define HorizontalCount 4
+/**   按钮的间距       the spide of every button    */
+#define TSBtnLableSpide 10.f
+/**   每一个按钮的宽度   the width of button       */
+#define TSBtnLableBtnW ([UIScreen mainScreen].bounds.size.width-((HorizontalCount+1)*TSBtnLableSpide))/HorizontalCount
+/**   按钮的高度        the height of every button  */
+#define TSBtnLableBtnH 40.f
 /**   按钮选择后的颜色   the color of selected button      */
 #define TSBtnSelectColor [UIColor redColor]
 /**   字体大小      size of the title         */
